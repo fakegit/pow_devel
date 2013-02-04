240ac7b45d64a2c80270ef11d2c56654779b1c11
khz <khz@tzi.org>
2013-02-04 18:27:54 +0100

    PowTable works now against MongoDB

--- stubs/lib/pow_mongodb_table.py
+++ stubs/lib/pow_mongodb_table.py
@@ -5,16 +5,30 @@ import string
 
 import pymongo
 
-sys.path.append( os.path.abspath(os.path.join__file__, "../lib" )))
+sys.path.append( os.path.abspath(os.path.join(__file__, "../lib" )))
 import powlib
-
-class PowTable(pymongo.collection):
-    
+from pow_mongodb_object import PowBaseObject
+class PowTable(pymongo.collection.Collection):
     """
-        Super Iinit: 
+        Super Init: 
             pymongo.collection.Collection(database, name[, create=False[, **kwargs]]])
             See: http://api.mongodb.org/python/current/api/pymongo/collection.html
     """
+    def __init__(self, name, create=False, **kwargs):
+        """initializes the collection with the according db and connection"""
+        pbo = PowBaseObject()
+        super(PowTable, self).__init__(pbo.get_db(), name, create, **kwargs)
+
+    def create_collection(self):
+        self.create_table()
+        return
+    def create_table(self):
+        self.database.createCollection(self.name)
+        return
+
+    def drop(self):
+        self.drop()
+
     def has_many(self, tablename):
         pass
     
@@ -29,13 +43,3 @@ class PowTable(pymongo.collection):
     
     def alter_column_name(self, colname, newname):
          pass
-         
-    def create_collection(self):
-        self.create_table()
-        return
-    def create_table(self):
-        self.database.createCollection(self.name)
-        return
-
-    def drop(self):
-        self.drop()
